7d5f9b904a02026b34524b4e6d7b8a04cfbe499b
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-09-16 22:58:13 +0700

    code register anh show data in textfield

--- app/src/main/java/com/example/singnlogin/MainActivity.java
+++ app/src/main/java/com/example/singnlogin/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.singnlogin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
+    TextView dangki;
+    TextView showText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,12 +34,23 @@ public class MainActivity extends AppCompatActivity {
 //        });
         username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        // Set input type to password
+        password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
         loginButton = findViewById(R.id.loginButton);
+        dangki= findViewById(R.id.dangki);
+        showText = findViewById(R.id.textShow);
+        dangki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, registerAccout.class));
+            }
+        });
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(username.getText().toString().equals("trucquynh") && password.getText().toString().equals("1234")) {
-                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                    showText.setText("UserName: "+ username.getText().toString() + "\nPassWord:" + password.getText().toString());
                 } else {
                     Toast.makeText(MainActivity.this,"Login Failed", Toast.LENGTH_SHORT).show();
                 }
